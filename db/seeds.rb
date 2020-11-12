cc589f91e7582a2197f7bdb166a2ca7a04ffe3f0
oliver coton <oliver.coton@gmail.com>
2020-11-12 17:28:56 +0100

    functioning mr cocktail with NO STYLE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Dose.create(description: "2 drops", cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: "a spoon", cocktail_id: 2, ingredient_id: 2)
+Dose.create(description: "lashings", cocktail_id: 3, ingredient_id: 1)
+
+Cocktail.create(name: "Margherita")
+Cocktail.create(name: "Snowball")
+Cocktail.create(name: "White Russian")
