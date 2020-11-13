2dd5954bafc4557c49fd45a537e16a9790000fa4
oliver coton <oliver.coton@gmail.com>
2020-11-13 16:44:19 +0100

    added image upload

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :address, :phone_number, :category)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
